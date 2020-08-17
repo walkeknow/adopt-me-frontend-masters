3622e726264c29c9437e3be9bd78a0f475f7c6d0
Kewal Shah <kewalshah077@gmail.com>
2020-08-17 15:16:48 +0530

    Use Hooks and async await

---
+++ src/SearchParams.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from './useDropdown';
+import Results from './Results'
+
+const SearchParams = () => {
+  const [location, setLocation] = useState('Seattle, WA');
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown('Breed', '', breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed('');
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStringList = breeds.map(({ name }) => name);
+      setBreeds(breedStringList);
+    }, console.error);
+  }, [animal, setBreed]);
+
+  return (
+    <div className="search-params">
+      <h1>{location}</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <label htmlFor="location">
+          Location
+          <input
+            type="text"
+            id="location"
+            placeholder="Location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
+
+export default SearchParams;
