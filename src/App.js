5afefc36358a4e2f5e2ee2081f7feaa55cba8f7b
Kewal <kewalshah077@gmail.com>
2020-08-30 11:42:57 +0530

    Revert "Add Reach Router"
    This reverts commit cbc25b2fbf887dd19cd10bf4f3d200474a11b91d.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Link } from '@reach/router';
 import SearchParams from './SearchParams';
-import Details from './Details';
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <header>
-          <Link to='/'>
-            Adopt Me!
-          </Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
+        <h1 id="something-important">Adopt Me!</h1>
+        <SearchParams />
       </div>
     </React.StrictMode>
   );
